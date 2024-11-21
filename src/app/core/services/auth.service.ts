62872a523d2c2c6ca7fab0597d54cd2258aa32de
Ezequiel Chorolque <ezequielchorolque14@gmail.com>
2024-11-21 20:27:02 -0300

    authentication services

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { User } from '../interfaces/user';
+import { signInWithPopup } from '@firebase/auth';
+import { signOut } from '@firebase/auth';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +10,34 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  /* user authentication */
+  getAuth() {
+    return getAuth()
+  }
+
+  /* user registration */
+  register(user: User) {
+    return createUserWithEmailAndPassword(getAuth(), user.email, user.password)
+  }
+
+  /* user login */
+  login(user: User) {
+    return signInWithEmailAndPassword(getAuth(), user.email, user.password)
+  }
+
+  /* user login with google */
+  loginInGoogle() {
+    return signInWithPopup(getAuth(), new GoogleAuthProvider())
+  }
+  /* user logout */
+  logout() {
+    return signOut(getAuth())
+  }
+
+  /* check if user is authenticated */
+  isAuthenticated(): boolean {
+    const user = getAuth().currentUser;
+    return user !== null
+  }
 }
