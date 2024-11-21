edeb18b6850512758b81bdb8d1d4d081450ccf1e
Ezequiel Chorolque <ezequielchorolque14@gmail.com>
2024-11-21 18:53:52 -0300

    firebase installation and configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,28 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { initializeApp } from 'firebase/app';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBpM0MbdL1bbe5AfKKOWstvZ4R6iz28ZzE",
+  authDomain: "login-firebase-159a5.firebaseapp.com",
+  projectId: "login-firebase-159a5",
+  storageBucket: "login-firebase-159a5.firebasestorage.app",
+  messagingSenderId: "246791805010",
+  appId: "1:246791805010:web:00ec1062895163b2cb678b"
+};
+
+initializeApp(firebaseConfig)
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),
+  importProvidersFrom(
+    HttpClientModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
+  )
+  ]
 };
