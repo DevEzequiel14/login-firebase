88c53fab62ce4001730aefb09aa4d73e11253328
Ezequiel Chorolque <ezequielchorolque14@gmail.com>
2024-11-22 19:06:20 -0300

    add login

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../core/services/auth.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { User } from '../../../core/interfaces/user';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  router = inject(Router);
+  authService = inject(AuthService);
+
+  form = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.authService.login(this.form.value as User)
+        .then(resp => this.router.navigate(['/dashboard']))
+        .catch(error => console.log(error));
+    }
+  }
+
+  loginInGoogle() {
+    this.authService.loginInGoogle()
+      .then(resp => this.router.navigate(['/dashboard']))
+      .catch(error => console.log(error));
+  }
 }
