221559c24ac691a7d5799e54fea51cb7190a10dc
Ezequiel Chorolque <ezequielchorolque14@gmail.com>
2024-11-22 18:19:12 -0300

    add user registration

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../core/services/auth.service';
+import { Router } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { User } from '../../../core/interfaces/user';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
+  loginService = inject(AuthService);
+  router = inject(Router);
+
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required])
+  });
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.loginService.register(this.form.value as User).then(resp => this.router.navigate(['/dashboard']));
+    }
+  }
 }
