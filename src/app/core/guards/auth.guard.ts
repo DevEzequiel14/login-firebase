773357885d10a45c46bb2fcae085f2461b95df4c
Ezequiel Chorolque <ezequielchorolque14@gmail.com>
2024-11-23 10:01:19 -0300

    add the guardian to check if the user is authenticated

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,5 +1,18 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+
+  const router = inject(Router);
+  const authService = inject(AuthService);
+  const isAuthenticated = authService.isAuthenticated();
+
+  if (isAuthenticated) {
+    return true;
+  }
+  else {
+    router.navigate(['/login']);
+    return false;
+  }
 };
