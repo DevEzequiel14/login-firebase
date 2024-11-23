7498d45a7b199171d38c380ab2ab69c3258aae48
Ezequiel Chorolque <ezequielchorolque14@gmail.com>
2024-11-23 20:48:00 -0300

    refactoring of routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,16 +3,21 @@ import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   {
-    path: 'dashboard',
-    canActivate: [authGuard],
-    loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent),
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/auth/login/login.component').then(m => m.LoginComponent),
   },
   {
     path: 'register',
     loadComponent: () => import('./pages/auth/register/register.component').then(m => m.RegisterComponent),
   },
   {
-    path: 'login',
-    loadComponent: () => import('./pages/auth/login/login.component').then(m => m.LoginComponent),
-  },
+    path: 'dashboard',
+    canActivate: [authGuard],
+    loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent),
+  }
 ];
